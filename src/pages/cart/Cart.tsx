ce4bf4e98f6cdb6e755ae2653e646e617a177cb6
Daniel Wolf Monaro <wolfmonaro@gmail.com>
2024-02-22 18:10:37 -0300

    funções de adicionar e remover itens

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,35 +1,71 @@
-export default () => {
-  return (
-    <div className="w-full max-w-7xl mx-auto">
-      <h1 className="font-semibold text-2xl text-center my-4">Meu carrinho</h1>
+import { useContext } from "react"
+import { CartContext, CartProps } from "../../contexts/CartContext"
+import { Link } from "react-router-dom"
 
-      <section className="flex items-center justify-between border-b-2 border-gray-300">
+export default () => {
+  const { cart, total, addItemCart, removeItemCart } = useContext(CartContext)
+
+  function renderCartItem(data: Array<CartProps>) {
+    if (data.length <= 0) {
+      return (
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-semibold">Ops seu carrinho está vazio...</p>
+          <Link
+            to='/'
+            className="bg-slate-600 my-3 p-1 px-3 text-white font-semibold rounded"
+          >
+            Acessar produtos
+          </Link>
+        </div>
+      )
+    }
+
+    return data.map((item) => (
+      <section key={item.id} className="flex items-center justify-between border-b-2 border-gray-300">
         <img
           className="w-28"
-          src="https://leonora.vteximg.com.br/arquivos/ids/161800/fone-de-ouvido-letron-earbud-bluetooth-preto_2.jpg?v=637729357675970000"
-          alt="Logo Produto"
+          src={item.cover}
+          alt={item.title}
         />
 
-        <strong>Preõ: R$ 1.000</strong>
+        <strong>Preço: {item.price}</strong>
 
         <div className="flex items-center justify-center gap-3">
-          <button className="bg-slate-600 px-2 rounded text-white font-semibold items-center justify-center"
+          <button
+            className="bg-slate-600 px-2 rounded text-white font-semibold items-center justify-center"
+            onClick={() => removeItemCart(item)}
           >
             -
           </button>
-          2
-          <button className="bg-slate-600 px-2 rounded text-white font-semibold items-center justify-center"
+          {item.amount}
+          <button
+            className="bg-slate-600 px-2 rounded text-white font-semibold items-center justify-center"
+            onClick={() => addItemCart(item)}
           >
             +
           </button>
         </div>
 
-        <strong className="float-right">
-          SubTotal: R$1000
+        <strong
+          className="float-right"
+        >
+          SubTotal:
+          {item.total.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          })}
         </strong>
       </section>
+    ))
+  }
 
-      <p className="font-bold mt-4">Total: R$ 1000</p>
+  return (
+    <div className="w-full max-w-7xl mx-auto">
+      <h1 className="font-semibold text-2xl text-center my-4">Meu carrinho</h1>
+
+      {renderCartItem(cart)}
+
+      {cart.length !== 0 && <p className="font-bold mt-4">Total: {total}</p>}
     </div>
   )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,30 +1,74 @@
+import { useContext, useEffect, useState } from "react"
 import { BsCartPlus } from "react-icons/bs"
+import { api } from "../../services/api"
+import { CartContext } from "../../contexts/CartContext";
+import toast from "react-hot-toast";
+
+export interface ProductsProps {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
 
 export default () => {
+  const { addItemCart } = useContext(CartContext)
+
+  const [products, setProducts] = useState<Array<ProductsProps>>([])
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get('/products')
+      setProducts(response.data)
+    }
+    getProducts()
+  }, [])
+
+  function renderProducts(data: Array<ProductsProps>) {
+    return data.map((product) => (
+      <section key={product.id} className="w-full">
+        <img
+          className="w-full rounded-lg max-h-70 mb-2 hover:scale-110 transition-all"
+          src={product.cover}
+          alt={product.title}
+        />
+        <p className="font-semibold mt-1 mb-2">{product.title}</p>
+
+        <div className="flex gap-3 items-center">
+          <strong className="text-zinc-700/90">
+            {product.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            })}
+          </strong>
+          <button className="bg-zinc-900 p-1 rounded">
+            <BsCartPlus size={20} color="#FFF" onClick={() => handleCartItem(product)} />
+          </button>
+        </div>
+
+      </section>
+    ))
+  }
+
+  function handleCartItem(product: ProductsProps) {
+    toast.success('Produto adicionado no carrinho', {
+      style: {
+        borderRadius: 10,
+        backgroundColor: '#121212',
+        color: '#FFF'
+      }
+    })
+    addItemCart(product)
+  }
+
   return (
     <div>
       <main className="w-full max-w-7xl px-4 mx-auto">
         <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
 
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-          <section className="w-full">
-            <img
-              className="w-full rounded-lg max-h-70 mb-2"
-              src="https://leonora.vteximg.com.br/arquivos/ids/161800/fone-de-ouvido-letron-earbud-bluetooth-preto_2.jpg?v=637729357675970000"
-              alt="Logo do Produto"
-            />
-            <p className="font-semibold mt-1 mb-2">Airpods Pro</p>
-
-            <div className="flex gap-3 items-center">
-              <strong className="text-zinc-700/90">
-                R$: 10
-              </strong>
-              <button className="bg-zinc-900 p-1 rounded">
-                <BsCartPlus size={20} color="#FFF"/>
-              </button>
-            </div>
-
-          </section>
+          {renderProducts(products)}
         </div>
       </main>
     </div>
